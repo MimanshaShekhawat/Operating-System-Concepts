3238ac60eed0e3a3ef6226c74312a5891f5bcfff
MimanshaShekhawat <83332350+MimanshaShekhawat@users.noreply.github.com>
2024-08-19 08:24:27 +0530

    Delete Q1.sh

--- Q1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-read -p "Enter username: " usrname
-var=$(cat /etc/passwd | grep "$usrname")
-if [[ -n $var ]]
-	then
-		echo "User '$usrname'  is present on the system"
-	else
-		echo "User '$usrname' does not exist"
-fi
