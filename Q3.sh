8b4038e00460fcdec421b3e18721d547a491093e
MimanshaShekhawat <83332350+MimanshaShekhawat@users.noreply.github.com>
2024-08-19 08:24:19 +0530

    Delete Q3.sh

--- Q3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-read -p "Enter username: " user
-var=$(cat /etc/passwd | grep "^$user\>" | cut -d':' -f3)
-if [[ -n $var ]]
-	then
-		echo "Userid is $var."
-        else
-		echo "User not present"
-fi
